63e4b0c7344ef8724733da00e368844f0a67bb6b
sasasukeee <134790706+sasasukeee@users.noreply.github.com>
2024-01-25 23:13:47 +0300

    app.js

--- app.js
+++ app.js
@@ -1,61 +1,116 @@
-const express = require("express");
-const app = express();
-const port = process.env.PORT || 3001;
+import { WebSocketServer } from 'ws';
+import { WebSocket } from "ws";
+import { randomBytes } from "crypto";
 
-app.get("/", (req, res) => res.type('html').send(html));
 
-const server = app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+function createBot(token, tokenid, recaptcha, RAWHOST, CUTHOST) {
+    try {
+        console.log("creating bot " + RAWHOST + " " + recaptcha);
 
-server.keepAliveTimeout = 120 * 1000;
-server.headersTimeout = 120 * 1000;
-
-const html = `
-<!DOCTYPE html>
-<html>
-  <head>
-    <title>Hello from Render!</title>
-    <script src="https://cdn.jsdelivr.net/npm/canvas-confetti@1.5.1/dist/confetti.browser.min.js"></script>
-    <script>
-      setTimeout(() => {
-        confetti({
-          particleCount: 100,
-          spread: 70,
-          origin: { y: 0.6 },
-          disableForReducedMotion: true
+        const socket = new WebSocket("wss://" + CUTHOST, {
+            headers: {
+                CUTHOST,
+                "connection": "Upgrade",
+                "pragma": "no-cache",
+                "cache-control": "no-cache",
+                "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
+                "upgrade": "websocket",
+                "origin": "https://" + CUTHOST + "/",
+                "sec-websocket-version": "13",
+                "accept-encoding": "gzip, deflate, br",
+                "accept-language": "en-US,en;q=0.9",
+                "sec-websocket-key": randomBytes(16).toString("base64"),
+                "sec-websocket-extensions": "permessage-deflate; client_max_window_bits"
+            }
         });
-      }, 500);
-    </script>
-    <style>
-      @import url("https://p.typekit.net/p.css?s=1&k=vnd5zic&ht=tk&f=39475.39476.39477.39478.39479.39480.39481.39482&a=18673890&app=typekit&e=css");
-      @font-face {
-        font-family: "neo-sans";
-        src: url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/l?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("woff2"), url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/d?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("woff"), url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/a?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("opentype");
-        font-style: normal;
-        font-weight: 700;
-      }
-      html {
-        font-family: neo-sans;
-        font-weight: 700;
-        font-size: calc(62rem / 16);
-      }
-      body {
-        background: white;
-      }
-      section {
-        border-radius: 1em;
-        padding: 1em;
-        position: absolute;
-        top: 50%;
-        left: 50%;
-        margin-right: -50%;
-        transform: translate(-50%, -50%);
-      }
-    </style>
-  </head>
-  <body>
-    <section>
-      Hello from Render!
-    </section>
-  </body>
-</html>
-`
+
+        socket.binaryType = "arraybuffer";
+
+        socket.onopen = function (event) {
+            console.log(token)
+            console.log(tokenid )
+            console.log("bot socket opened");
+            socket.send(JSON.stringify([
+                "Epo",
+                2120,
+                1280,
+                52,
+                token,
+                tokenid,
+                0,
+                0,
+                0,
+                0,
+                0,
+                1,
+                0,
+                0,
+                0,
+                null,
+                "🎁",
+                recaptcha
+            ]));
+
+
+        }
+
+        socket.onmessage = function (event) {
+
+            if (typeof event.data == "string") {
+                let msg = JSON.parse(event.data);
+            } else {
+                let ui8 = new Uint8Array(event.data);
+                switch (ui8[0]) {
+                    case 16:
+                        console.log(ui8[2])
+                        if(ui8[2] < 80) {
+                            socket.send(JSON.stringify([5, 104]))
+                        }
+                }
+            }
+        }
+        // socket.onerror = function (event) {
+        //     console.log("socket error");
+        // }
+        // socket.onclose = function () {
+        //     console.log("ws closed");
+        // }
+
+
+    } catch (e) {
+        console.log(e);
+    }
+}
+
+
+
+
+let STARVE_TOKEN = "";
+let STARVE_TOKEN_ID = "";
+
+
+let port = 8080;
+const wss = new WebSocketServer({ port: port });
+
+console.log("listening on port " + port)
+wss.on('connection', function connection(ws) {
+    console.log("connected to script")
+
+    ws.on('message', function message(data) {
+        let packet = JSON.parse(data);
+        switch (packet[0]) {
+            case "join":
+                let recaptcha = packet[1];
+                let RAWHOST = packet[2];
+                let CUTHOST = packet[3];
+                
+                createBot(STARVE_TOKEN, STARVE_TOKEN_ID, recaptcha, RAWHOST, CUTHOST)
+                break;
+            case "tokens":
+                STARVE_TOKEN = packet[1];
+                STARVE_TOKEN_ID = packet[2];
+                break;
+        }   
+
+    });
+});
